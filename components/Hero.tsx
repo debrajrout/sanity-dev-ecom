758953cf4104d64660487c95763e5c3784ac3c26
debrajrout <debrajrout28@gmail.com>
2024-01-26 12:27:22 +0530

    Add Link component and update image source

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { client, urlFor } from "@/app/lib/sanity";
+import Link from "next/link";
 
 async function getData() {
   const query = "*[_type == 'heroImage'][0]";
@@ -22,9 +23,9 @@ export default async function Hero() {
           </p>
         </div>
         <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
-          <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
+          <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-xl md:left-16 md:top-16 lg:ml-0">
             <Image
-              src={urlFor(data.image1).url()}
+              src={urlFor(data.image2).url()}
               alt="product1"
               className="h-full w-full object-cover object-center"
               priority
@@ -32,6 +33,38 @@ export default async function Hero() {
               height={500}
             />
           </div>
+          <div className="overflow-hidden rounded-lg bg-gray-100 shadow-xl">
+            <Image
+              src={urlFor(data.image1).url()}
+              alt="Great Photo"
+              className="h-full w-full object-cover object-center"
+              width={500}
+              height={500}
+              priority
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
+        <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
+          <Link
+            href="/Men"
+            className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
+          >
+            Men
+          </Link>
+          <Link
+            href="/Women"
+            className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
+          >
+            Women
+          </Link>
+          <Link
+            href="/Kids"
+            className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
+          >
+            Kids
+          </Link>
         </div>
       </div>
     </section>
