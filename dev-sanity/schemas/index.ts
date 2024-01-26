6973a689e946c66f0ed30f7691f47e5005e8b3ba
debrajrout <debrajrout28@gmail.com>
2024-01-26 11:49:05 +0530

    Remove unused files and dependencies, add Navbar and Hero component

--- dev-sanity/schemas/index.ts
+++ dev-sanity/schemas/index.ts
@@ -1,6 +1,5 @@
 import catagory from './catagory'
 import heroImages from './heroImages'
-import heroimages from './heroImages'
 import product from './product'
 
 export const schemaTypes = [product, catagory, heroImages]
