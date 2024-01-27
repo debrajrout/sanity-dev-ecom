7828293608d1fc4c0828cf36584f3c9e022ddd4a
debrajrout <debrajrout28@gmail.com>
2024-01-27 22:18:33 +0530

    Reorder imports in sanity.config.ts

--- dev-sanity/sanity.config.ts
+++ dev-sanity/sanity.config.ts
@@ -1,7 +1,8 @@
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {structureTool} from 'sanity/structure'
 
 export default defineConfig({
   name: 'default',
